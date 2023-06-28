649e8d9564406f50fe73d263d45e3d330c72c042
Gyedu1 <gyedu@DESKTOP-G9GQKPD.localdomain>
2023-06-28 13:02:30 +0100

    multiple mains corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,14 @@
 #include "main.h"
-
 /**
- * main - Entry point of the program
- *
- * This function serves as the entry point for the program. It initializes
- * integer variable 'i' with value 0 and calls the 'positive_or_negative'
- * function with 'i' as an argument. The function 'positive_or_negative' checks
- * if the number is positive,negative or zero and displays appropriate message.
- *
- * Return: Always 0 (indicating successful execution)
+ * main - Test function for positive or negative
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
-	return (0);
+return (0);
 }
 
